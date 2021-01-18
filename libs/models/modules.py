65de70e14d56ad8519e407cd0b08ee4d7ce38c3b
jo-kwsm <ytk.kwsm@gmail.com>
2021-01-18 17:50:25 +0900

    リファクタリング

--- libs/models/modules.py
+++ libs/models/modules.py
@@ -11,18 +11,23 @@ def make_vgg():
     
     for v in config:
         if v=="M":
-            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+            layers.append(nn.MaxPool2d(kernel_size=2, stride=2))
         elif v=="MC":
-            layers += [nn.MaxPool2d(kernel_size=2, stride=2, ceil_mode=True)]
+            layers.append(nn.MaxPool2d(kernel_size=2, stride=2, ceil_mode=True))
         else:
             conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
-            layers += [conv2d, nn.ReLU(inplace=True)]
+            layers.append(conv2d)
+            layers.append(nn.ReLU(inplace=True))
             in_channels = v
 
     pool5 = nn.MaxPool2d(kernel_size=3, stride=1, padding=1)
     conv6 = nn.Conv2d(512, 1024, kernel_size=3, padding=6, dilation=6)
     conv7 = nn.Conv2d(1024, 1024, kernel_size=1)
-    layers += [pool5, conv6, nn.ReLU(inplace=True), conv7, nn.ReLU(inplace=True)]
+    layers.append(pool5)
+    layers.append(conv6)
+    layers.append(nn.ReLU(inplace=True))
+    layers.append(conv7)
+    layers.append(nn.ReLU(inplace=True))
 
     return nn.ModuleList(layers)
 
