bdc3fd221c163bfc4bd098f45271f065edca8bd1
jo-kwsm <ytk.kwsm@gmail.com>
2021-01-22 12:09:24 +0900

    bug fixed

--- libs/class_id_map.py
+++ libs/class_id_map.py
@@ -7,7 +7,7 @@ def get_cls2id_map() -> Dict[str, int]:
         "bottle": 4, "bus": 5, "car": 6, "cat": 7,
         "chair": 8, "cow": 9, "diningtable": 10, "dog": 11,
         "horse": 12, "motorbike": 13, "person": 14, "pottedplant": 15,
-        "sheep": 16, "sofa": 17, "train": 18, "tvmonitor": 18
+        "sheep": 16, "sofa": 17, "train": 18, "tvmonitor": 19
     }
     return cls2id_map
 
